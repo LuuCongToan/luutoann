91c9d8fb5dbd742e7583bc4764f87dd9a3aa6987
LuuCongToan <103622400+LuuCongToan@users.noreply.github.com>
2024-03-08 11:44:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '💘Emiu💘';
+const giftUrl = 'https://www.tiktok.com/@luuton02';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc eiu của anh 8/3 tràn ngập niềm vui và những nụ cười💘. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau💘';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
